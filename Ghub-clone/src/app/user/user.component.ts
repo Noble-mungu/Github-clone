2960200b580c50ca7637f2a67c2f584ebe44a4cc
Noble Mungu <mungunoble@gmail.com>
2020-08-31 21:44:24 +0300

    add finduser and search user funtion

--- Ghub-clone/src/app/user/user.component.ts
+++ Ghub-clone/src/app/user/user.component.ts
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+ public searchMe = 'Mungu';
+    public githubUser: string;
 
-  ngOnInit(): void {
+    users: User ;
+    repository: Repository;
+    public searchRepo: string;
+    public resultCount = 12;
+
+
+    findUser(username) {
+        this.githubUser = '';
+        this.searchMe  = username;
+        this.ngOnInit();
+    }
+
+
+  constructor(public githubUserRequest: SearchRequestService, public userRepos: SearchRequestService) { }
+
+  ngOnInit() {
+      this.githubUserRequest.githubUser(this.searchMe);
+      this.users = this.githubUserRequest.users;
+      this.userRepos.gitUserRepos(this.searchMe);
+      console.log(this.userRepos);
   }
 
+
+    searchRepos() {
+        this.searchRepo = '';
+        this.resultCount = 10;
+
+    }
 }
