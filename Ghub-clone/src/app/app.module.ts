53b235f300aa31ad88bd5bc2ef4bf2b1e94885fc
Noble Mungu <mungunoble@gmail.com>
2020-08-31 21:58:54 +0300

    add navigation component

--- Ghub-clone/src/app/app.module.ts
+++ Ghub-clone/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
-    NavbarComponent
+    NavbarComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule
