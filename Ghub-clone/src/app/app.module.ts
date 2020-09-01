fea086fbcb14c1348583bf41d7cdfbe9d0119e52
Noble Mungu <mungunoble@gmail.com>
2020-09-01 14:41:33 +0300

    add repositrty class

--- Ghub-clone/src/app/app.module.ts
+++ Ghub-clone/src/app/app.module.ts
@@ -1,28 +1,47 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {RepositoriesComponenet } from './repositories/repositories.component';
+// import {HttpClient} from '@angular/common/http';
+import {HttpClientModule} from '@angular/common/http';
+import { NgProgressModule } from '@ngx-progressbar/core';
+import { NgProgressHttpModule } from '@ngx-progressbar/http';
+
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarComponent } from './navigation/navbar.component';
+import { SearchComponent } from './user/search.component';
+import { RepositoriesComponent } from './repositories/repositories.component';
+import {RoutingModule} from './routing/routing.module';
 import { RouterModule } from '@angular/router';
-import {FormsModule } from '@angular/forms';
 import { SearchFormComponent } from './search-form/search-form.component';
-import { HttpClientmodule } from '@angular/common/http';
-import {SearchRequestService } from './search-request.service';
+import {FormsModule} from '@angular/forms';
+import {SearchRequestService} from './search-request.service';
+import { UpperCasePipe } from './upper-case.pipe';
+
+
+
+
+
+
+
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    Repositories,
-    SearchFormComponent
+    SearchComponent,
+    RepositoriesComponent,
+    SearchFormComponent,
+    UpperCasePipe
   ],
   imports: [
     BrowserModule,
-    Routingmodule,
-    FormsModule,
-    HttpClientmodule
+      RoutingModule,
+      RouterModule,
+      FormsModule,
+      NgProgressModule.forRoot(),
+      NgProgressHttpModule,
+      // HttpClient,
+      HttpClientModule
   ],
-  providers: [],
-  providers:[SearchRequestService],
+  providers: [SearchRequestService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
